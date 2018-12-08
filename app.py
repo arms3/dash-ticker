39f37a4c72f0c65e23e37e190925a832db8bb934
Angus Sinclair <sinclair.angus@gmail.com>
2018-12-07 16:35:50 -0800

    Added Navbar

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import pandas as pd
 external_stylesheets = [
     'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
     'https://fonts.googleapis.com/css?family=Montserrat:400,700']
+
 server = flask.Flask(__name__)
 server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
 app = dash.Dash(__name__, server=server,
@@ -19,36 +20,49 @@ app = dash.Dash(__name__, server=server,
 df = fetch('AAPL')
 ticks = get_tickers()
 
-app.layout = html.Div(children=[
-    html.H1('Stock Price Explorer'),
-    dcc.Dropdown(
-        id='example-dropdown',
-        options=[{'label': r[2], 'value': r[0]} for r in ticks.values],
-        value=['AAPL'],
-        multi=True,
-    ),
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [{'x': df.date, 'y': df.adj_close, 'type': 'line', 'name':
-                      'AAPL'}]
-        }
-    ),
-    dash_table.DataTable(
-        id='table',
-        columns=[{"name": i, "id": i} for i in df.columns],
-        data=df.to_dict("rows"),
-        # pagination_settings = {'page_size': 20, 'current_page': 0},
-        style_cell={'textAlign': 'left', 'padding': '5px',
-                    'font-family': 'Montserrat'},
-        style_table={
-            'maxHeight': '350px',
-            # 'overflowY': 'scroll'
-        }
-    ),
-    # Hidden div inside the app that stores the intermediate value
-    html.Div(id='intermediate-value', style={'display': 'none'}),
-], className="container")
+app.layout = html.Div([
+    # Navbar
+    html.Nav(children=[
+        html.Div([
+            html.Div(html.A('Home', className='navbar-brand', href='/'), className='navbar-header'),
+            html.Div(html.Ul(html.Li(html.A('About', href='/about')), className='nav navbar-nav navbar-right')),
+        ], className='container')
+    ], className="navbar navbar-inverse navbar-static-top"),
+
+    html.Div([
+
+        html.H1('Stock Price Explorer'),
+        dcc.Dropdown(
+            id='example-dropdown',
+            options=[{'label': r[2], 'value': r[0]} for r in ticks.values],
+            value=['AAPL'],
+            multi=True,
+        ),
+
+        dcc.Graph(
+            id='example-graph',
+            figure={
+                'data': [{'x': df.date, 'y': df.adj_close, 'type': 'line', 'name': 'AAPL'}]
+            }
+        ),
+
+        dash_table.DataTable(
+            id='table',
+            columns=[{"name": i, "id": i} for i in df.columns],
+            data=df.to_dict("rows"),
+            # pagination_settings = {'page_size': 20, 'current_page': 0},
+            style_cell={'textAlign': 'left', 'padding': '5px',
+                        'font-family': 'Montserrat'},
+            style_table={
+                'maxHeight': '350px',
+                # 'overflowY': 'scroll'
+            }
+        ),
+
+        # Hidden div inside the app that stores the intermediate value
+        html.Div(id='intermediate-value', style={'display': 'none'}),
+    ], className="container"),
+])
 
 
 @app.callback(
